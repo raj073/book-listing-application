e3a6e2b4c5619c746455e2e850cd26b221047277
raj073 <rajoni.das@outlook.com>
2023-09-28 23:11:38 +0600

    Handle Not Found Added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+//app.use("/api/v1", routes);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
