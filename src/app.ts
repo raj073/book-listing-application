516792694b0e952c864d40bdd5450ca786245fb3
raj073 <rajoni.das@outlook.com>
2023-09-28 21:47:25 +0600

    app.ts and index.ts added

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import cors from "cors";
+import express, { Application } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
